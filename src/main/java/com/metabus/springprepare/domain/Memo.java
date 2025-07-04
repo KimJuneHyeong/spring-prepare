c34399ade2b5fba01e2c872b43960d1310050cf1
june <thanks7229@naver.com>
2025-07-04 10:32:41 +0900

    feat: Lombok 애너테이션 활용 2 * @NoArgsConstructor 기본 생성자 애너테이션 * @AllArgsConstructor 모든 필드를 받는 애너테이션

--- src/main/java/com/metabus/springprepare/domain/Memo.java
+++ src/main/java/com/metabus/springprepare/domain/Memo.java
@@ -1,10 +1,15 @@
 package com.metabus.springprepare.domain;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-//@Setter
+@Setter // 값 변경
+@NoArgsConstructor// 기본 생성자 생성 애너테이션
+@AllArgsConstructor
 public class Memo {
     private String username;
     private String content;
