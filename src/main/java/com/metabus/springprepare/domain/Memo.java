1d92a1086382c0602a68931ba8719ea7df5de4c4
june <thanks7229@naver.com>
2025-07-04 10:35:40 +0900

    feat: Lombok 애너테이션 활용 3 * @RequireArgsConstructor final 제어자가 붙은 필드를 가져야하는 생성자

--- src/main/java/com/metabus/springprepare/domain/Memo.java
+++ src/main/java/com/metabus/springprepare/domain/Memo.java
@@ -1,19 +1,17 @@
 package com.metabus.springprepare.domain;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter // 값 변경
-@NoArgsConstructor// 기본 생성자 생성 애너테이션
-@AllArgsConstructor
+//@Getter
+//@Setter // 값 변경
+//@NoArgsConstructor// 기본 생성자 생성 애너테이션
+//@AllArgsConstructor
+@RequiredArgsConstructor
 public class Memo {
-    private String username;
+    private final String username;
     private String content;
     private String description;
-    private String password;
+    private final String password;
 
 }
